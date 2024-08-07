57ba1ba92f6592b676d59218c7ad43785a247029
jdeveloperanalyst <jonatas1200@icloud.com>
2024-08-06 21:18:24 -0300

    hotfix/desafio 21 + mp3

---
+++ Python3 Desafios/Desafios Scripts Python3 Mundo 1/ex021.py
@@ -0,0 +1,13 @@
+# Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+
+import pygame
+
+pygame.mixer.init()
+pygame.mixer.music.load(
+    "/Users/jonatasoliveira/Documents/Python Projects/Curso-em-Video/"
+    "Python3 Desafios/Desafios Scripts Python3 Mundo 1/ex021.mp3"
+)
+pygame.mixer.music.play()
+
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
