e2f22bb241770748d22b2c1d8a76d606b4279ee2
jdeveloperanalyst <jonatas1200@icloud.com>
2025-02-17 11:40:29 -0300

    hotfix/desafio45

--- Python3 Desafios/Desafios Scripts Python3 Mundo 2/desafio045.py
+++ Python3 Desafios/Desafios Scripts Python3 Mundo 2/desafio045.py
@@ -1,45 +1,50 @@
 from random import randint
+from time import sleep
 
 print(
-    """[1] - PEDRA
+    """Suas opções:
+[1] - PEDRA
 [2] - PAPEL
-[3] - TESOURA
-Qual a sua jogada? """
+[3] - TESOURA"""
 )
 
 itens = [None, "PEDRA", "PAPEL", "TESOURA"]
 t800 = randint(1, 3)
 jogador = int(input("Qual é a sua jogada? "))
-print(itens[jogador])
-print(itens[t800])
+# print(itens[jogador])
+# print(itens[t800])
+
+sleep(1)
+print("JO")
+sleep(1)
+print("KEN")
+sleep(1)
+print("PO!!!")
+sleep(1)
+
+print("-=" * 11)
+print(f"Você jogou {itens[jogador]} \ne a maquina {itens[t800]}!")
+print("-=" * 11)
 
 if itens[jogador] == "PEDRA":
     if itens[t800] == "PEDRA":
         print("EMPATOU...")
-        print(f"Você Jogou {itens[jogador]} e a maquina {itens[t800]}!")
     elif itens[t800] == "PAPEL":
         print("VOCÊ PERDEU...")
-        print(f"Você jogou {itens[jogador]} e a maquina {itens[t800]}!")
     elif itens[t800] == "TESOURA":
         print("VOCÊ GANHOU...")
-        print(f"Você jogou {itens[jogador]} e a maquina {itens[t800]}!")
 elif itens[jogador] == "PAPEL":
     if itens[t800] == "PAPEL":
         print("EMPATOU...")
-        print(f"Você Jogou {itens[jogador]} e a maquina {itens[t800]}!")
     elif itens[t800] == "TESOURA":
         print("VOCÊ PERDEU...")
-        print(f"Você jogou {itens[jogador]} e a maquina {itens[t800]}!")
     elif itens[t800] == "PEDRA":
         print("VOCÊ GANHOU...")
-        print(f"Você jogou {itens[jogador]} e a maquina {itens[t800]}!")
 elif itens[jogador] == "TESOURA":
     if itens[t800] == "TESOURA":
         print("EMPATOU...")
-        print(f"Você Jogou {itens[jogador]} e a maquina {itens[t800]}!")
     elif itens[t800] == "PEDRA":
         print("VOCÊ PERDEU...")
-        print(f"Você jogou {itens[jogador]} e a maquina {itens[t800]}!")
     elif itens[t800] == "PAPEL":
         print("VOCÊ GANHOU...")
-        print(f"Você jogou {itens[jogador]} e a maquina {itens[t800]}!")
+print()
